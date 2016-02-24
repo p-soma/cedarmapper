0a8f39df3e22a797048fff62084f9d3834a2be23
Pat Bills <pat@billspat.com>
2016-02-23 23:17:36 -0500

    almost finished outline of CEDAR framework for circular data

--- R/cedarFunctions.R
+++ R/cedarFunctions.R
@@ -3,67 +3,184 @@
 
 # generate a set of points on circle
 # example c = randCircle(); plot(c)
+library(cluster)
+library(igraph)
 
-randCircle <- function(r=1, n=60) {
-  angles = sample(359,n)
-  c = data.frame(X=cos(angles)*r, Y=sin(angles)*r)
-  return(c)
-}
-
-ylense <- function(rowdata) {
-  # 4 groups and 50% overlap
+main <- function(make_plots=FALSE) {
   
-  ngroups=4
-  o=0.5
-  # this function needs to be made generic 
-  partition_size = 0.4
-  lense.function = function(x) {
-    return(data.frame(L=x$Y, ID=x$ID))
-  }
-  
-
-  # partition_size = round(((nrow(rowdata)/ngroups ) * (1 + o)) + 0.5)
-  
-  partition_size = round ( ( nrows(rowdata) * 0.4 ) + 0.5 ) 
-
-  lense.partition = function(s) {
-    return(lense.d[s:(s+group_size),])
-  
-  }
-  
-  # make sure it's a data frame, and add an ID number
-  c.df = data.frame(c,ID=seq.int(n))
+        # d is for data
+  d = circle.data(r=1,n=60)
+        # lense partitions
+  d.partitions = circle.partition(d, l = 4)
+        # list of clusters using euclidean distance, single linkage, and  gap clustering detection, 
+  d.clusters = circle.clusters(d, d.partitions)
+        # from clusters create nodes of sets of d
+  d.nodes  = circle.nodes(d,d.clusters)
+        # look for links and build adjacency_matrix
+  adjacency_matrix = circle.adj(d.nodes)
+        # create an edge list
+  d.graph = circle.graph(adjacency_matrix) 
+  plot(d.graph)
+        # visualize
+  # htmlwidget goes here, converting edge list to JSON format
  
-  lense.d = lense.function(c)
-  
-  
-  
-  # sort result from lense function
-  lense.d = lense.d[with(lense.d, order(L)), ]
-  
-  # calculate size of partitions
-  interval =  group_size * (1-o)
+  if(make_plots) {
+    # circle
+    plot(d$X, d$Y)
+    
+    # plot just the partitions
+    o_par = par()  # par == R's plot parameter structure, save original
+    par(mfrow=c(2,2)) # set for 2X2 plot
+    for(i in 1:4){ with(d.partitions[[i]], plot(X,Y)) }
+    par(mfrow=c(1,1))
+    # dendograms and optimal cluster K
+    circle_cluster_gap_viz(d,d.partitions)
+    
+    plot(graph.adjacency(adjacency_matrix, mode ="undirected",weighted=TRUE))
 
-   # to do
-  # this doesn't cover all 60 items
-  # this doesn't accumulate the groups
-
-  lense.lists = lapply((1:ngroups),lense.partition)
+  } 
+  
+  return(edgelist)
   
 }
 
 
-discoverLinks <- function(x){
-  # given x = list of subsets of a data source
-  #  [ 1 = (3,4,5),2 =  (4,5,6,7), 3= (1,2,3), 4=(5,4,2)]
-  # discover which subsets share values 
-  # and output list of links
-  # 1, 2
-  # 1, 3
-  # 1, 4
-  # 2, 4
-  # 3, 4
-
+######DATA
+circle.data <- function(r=1, n=60, randomize=FALSE) {
+  if (randomize){
+    angles = (runif(n, -1*r, 1*r)) * pi
+  } else {
+    # evenly spaced arcs
+    angles = (0:(n-1))*2*(pi/n)    
   }
+  
+  d = data.frame(X=cos(angles)*r, Y=sin(angles)*r)
+  # add an ID number
+  d = data.frame(d,ID=seq.int(n))
+  rownames(d) = d$ID
+  return(d)
+}
 
+# partitioning using the Y column only, 4 groups essentially built in, 
+# even though there is a parameter here it's not really used. 
+circle.partition <- function(d, l = 4) {
+  
+  # our simple lense function
+  circle.lense = function(d) {
+    lense_data = data.frame(L=d$Y, ID=seq.int(nrow(d)))
+    return(lense_data)
+  }
+  
+  # lense function here returns data frame with column L
+  # could be a simple array or vector at this point with named rows
+  lense.df = circle.lense(d) 
+  
+  # simple partitioning paritions with 50% overlap; 
+  partition_size = 2 * ( max(lense.df$L) - min(lense.df$L))/(l+1)  # l = num of partitions
+  
+  # ? sort result from lense function
+  # lense.df = lense.df[with(lense.df, order(L)), ]
+  partitions = list()
+  lenses = list()
+  for (i in 1:l) {
+    offset= partition_size * (i - 1) * 0.5  # offset is 1/2 partition size X parttion number
+    lenses[[i]] = lense.df[(lense.df$L >= (min(lense.df$L) + offset)) & (lense.df$L <= (min(lense.df$L) + partition_size+offset)),]
+    partitions[[i]] = d[lenses[[i]]$ID,]
+    
+  }
+  # to test that all rows are present
+  # nrow(d) == length(sort(unique(c(lense.list[[1]]$ID, lense.list[[2]]$ID,lense.list[[4]]$ID, lense.list[[3]]$ID))))
+  return(partitions)
+} 
+
+# single linkage clustering but no cluster identification method here, see gaps method below
+# this function is getting obsolete
+circle.old.clusters <- function(d, partitions){
+  l = length(partitions)
+  clusters = list()
+  for ( i in 1:l) {
+      d.subset = d[d$ID==partitions[[i]]$ID,-3]
+      d.dist = dist(d.subset, method="euclidean")
+      clusters[[i]] = hclust(d.dist, method = "single")
+  }  
+  return(clusters) 
+}
+
+circle_cluster_gap_viz <- function(d,partitions){
+  l = length(partitions)
+  o_par = par()
+  par(mfrow = c(2,2))
+
+  for ( i in 1:l) {
+    # print dendograms with colored clusters
+    
+    d.subset = partitions[[i]][,-3]  # remove the ID column,TODO remove hard coded col num
+    eClusts = eclust(d.subset, FUNcluster="hclust", k.max = 5, stand =TRUE, B = 500, hc_metric="euclidean", hc_method="single")
+    # don't keep the value, just plot
+  }
+  
+  for ( i in 1:l) {
+    cGaps <- clusGap(as.matrix(d.subset), FUN = hcut, K.max = 5, B = 200, hc_metric="euclidean", hc_method="single")
+    print(fviz_gap_stat(cGaps ))
+  }
+  par(o_par)
+
+}
+# cluster detection using NbClust; 
+# the clustering built in so works on partitions
+circle.clusters<- function(d,partitions) {
+  l = length(partitions)
+  nbClusts = list()
+  
+  for ( i in 1:l) {
+    d.subset = partitions[[i]][,-3]  # remove the ID column,TODO remove hard coded col num
+    nb = NbClust(as.matrix(d.subset), distance = "euclidean", method="single",
+            min.nc = 1, max.nc = 5,index = "gap")
+    nbClusts[[i]] = nb$Best.partition
+  }
+  return(nbClusts)
+}
+
+
+circle.nodes<- function(d,clusters){
+  l = length(clusters)
+  nodes = list()
+  node_counter = 0
+  
+  cuts = clusters[[i]]
+  for ( i in 1:l) {
+    for(j in unique(cuts)){
+      node_ids = d[names(cuts[cuts == j]), ]
+      node_counter = node_counter + 1
+      nodes[[node_counter]] = d[names(cuts[cuts == j]), ]
+    }
+  }
+  return(nodes)
+}
+
+
+circle.adj<- function(nodes) {
+  # function that tells if there is overlapping rows of data
+  detect_overlap <- function(a,b) { length(intersect(a$ID,b$ID)) }
+  
+  adjmat <- matrix(0, ncol = length(nodes), nrow = length(nodes))
+  colnames(adjmat) <- rownames(adjmat) <- names(nodes)
+  for(i in 1:nrow(adjmat)) {
+    for(j in 1:ncol(adjmat)) {
+      if (i == j) {
+        adjmat[i,j] = 0
+      } else {adjmat[i,j] <- detect_overlap(nodes[[i]], nodes[[j]])}
+    }
+  }
+  return(adjmat)
+}
+
+circle.graph<- function(adjmatrix){
+  # need to use just upper half of matrix
+  adjmatrix[lower.tri(adjmatrix)] <- 0
+  
+  g  <- graph.adjacency(adjmatrix, mode ="undirected",weighted=TRUE)
+
+  return(g)
+}
 
