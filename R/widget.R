4b6323b7a071fd20b697488b5bef30829c46d3e4
Pat Bills <pat@billspat.com>
2016-03-09 03:00:37 -0500

    support functions to display graph widget

---
+++ R/widget.R
@@ -0,0 +1,44 @@
+# graph widget demonstration and support functions
+
+# source("R/cedarFunctions.R")
+
+library(htmlwidgets)
+
+nodePrep = function(nodes){
+  ## get node data ready for js widget
+  
+  #### prep nodes -- currently this only works with circle data
+  # TO DO: introspect columns in nodes and add means for all columns
+  
+  meanx <- function(litem) { mean(litem$X)}
+  nodes = data.frame("name"  = as.vector((V(g))), 
+                     "values"= unlist(lapply(nodes,meanx)))
+  return(nodes)
+}
+
+
+linkPrep <- function(nodes){
+  ## get link data for js widget
+  # requires cedar functions to work
+  
+  adjmatrix = cedar.adj(nodes)
+  # create an edge list
+  g = cedar.graph(adjmatrix)
+  links = get.data.frame(g)
+  # make it work for javascript zero-based indexing
+  links = data.frame(source=links$from - 1, target = links$to - 1 , weight = links$weight)
+  
+  return(links)
+
+}
+
+saveNodes <- function(nodes, filename="cedarcircle.rda"){
+  circle.links = linkPrep(nodes)
+  circle.nodes = nodePrep(nodes)
+  save(circle.links, circle.nodes, file=paste("data",filename,sep="/"))
+}
+
+# note this doesn't seem to work unless run from console
+nodes = main(100)
+simpleGraph(linkPrep(nodes), nodePrep(nodes))
+
