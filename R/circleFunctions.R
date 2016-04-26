94e2b699bd0b0e2a007bbd44b04f0124915be92d
Pat Bills <pat@billspat.com>
2016-04-26 14:28:28 -0400

    refactor in progress for Node code

---
+++ R/circleFunctions.R
@@ -0,0 +1,71 @@
+# functions to work with simply circle data
+test<- function(npoints=100) {
+  
+  makegraphmapper(circle_data(1, npoints), circle_lense, partition_count=4, overlap = 0.5, partition_method="single", index_method="gap")
+  
+}
+# generate a set of points on circle
+# example c = randCircle(); plot(c)
+#' @export
+circle_data <- function(r=1, n=60, randomize=FALSE) {
+  if (randomize){
+    angles = (runif(n, -1*r, 1*r)) * pi
+  } else {
+    # evenly spaced arcs
+    angles = (0:(n-1))*2*(pi/n)    
+  }
+  
+  d = data.frame(X=cos(angles)*r, Y=sin(angles)*r)
+  # add an ID number
+  d = data.frame(d,ID=seq.int(n))
+  rownames(d) = d$ID
+  return(d)
+}
+
+# partitioning using the Y column only, 4 groups essentially built in, 
+# even though there is a parameter here it's not really used. 
+
+
+
+# our simple lense function
+#' @export
+circle_lense = function(d) {
+  lense_data = data.frame(L=d$Y, ID=rownames(d), stringsAsFactors = FALSE)
+  rownames(lense_data) = rownames(d)
+  return(lense_data)
+}
+
+circlegraph <- function(r=1,n=npoints, randomize=FALSE){
+  d = circle.data(r,n,randomize)  # data frame with X & Y values
+  # gm  = graphmapper(d, ) etc
+}
+
+#' @export
+circle_cluster_gap_viz <- function(d,partitions){
+  l = length(partitions)
+  o_par = par()
+  par(mfrow = c(2,2))
+  
+  for ( i in 1:l) {
+    # print dendograms with colored clusters
+    
+    d.subset = partitions[[i]][,-3]  # remove the ID column,TODO remove hard coded col num
+    eClusts = eclust(d.subset, FUNcluster="hclust", k.max = 5, stand =TRUE, B = 500, hc_metric="euclidean", hc_method="single")
+    # don't keep the value, just plot
+  }
+  
+  for ( i in 1:l) {
+    cGaps <- clusGap(as.matrix(d.subset), FUN = hcut, K.max = 5, B = 200, hc_metric="euclidean", hc_method="single")
+    print(fviz_gap_stat(cGaps ))
+  }
+  par(o_par)
+  
+}
+
+#' @export
+circleapp <- function(){
+  shiny::runApp(system.file('circleapp.R', package='cedar'))
+}
+
+
+
---
+++ R/nodeFunctions.R
@@ -0,0 +1,232 @@
+# cedarFunctions.R
+# cedar project development functions
+#' @import cluster
+#' @import NbClust
+#' @import igraph
+
+# for gap and colored dendograms, optional
+library(factoextra)
+# to install the above requires 'Hmisc' which requires binary install on Mac
+# see https://cran.r-project.org/web/packages/Hmisc/index.html 
+
+
+# graphmapper class factory
+# this prepares the data structure, then runs each mapper step
+#' @export
+makegraphmapper <- function(x, lensefun, partition_count=4, overlap = 0.5, partition_method="single", index_method="gap"){
+  gm = structure(list(d = x, 
+                      "lense_count"=lense_count, 
+                      "overlap" = overlap,   # percent, o <= 1
+                      "lensefun"=lensefun, 
+                      "partition_method"=partition_method, 
+                      "index_method"=index_method), 
+                 class="graphmapper")
+  
+  
+  partition.graphmapper(gm)
+  
+  # list of clusters using euclidean distance, single linkage, and  gap clustering detection, 
+  gm[["clusters"]]   = cedar.clusters(gm)
+  
+  # from clusters create nodes of sets of d
+  gm[["nodes"]]     = cedar.nodes(gm)
+  
+  gm[["adjmatrix"]] = cedar.adj(d.nodes) 
+  return(gm)
+  
+}
+
+#' @export
+partition <- function(d, lensefun, l, o){
+ 
+  # calculate lense values for all rows
+  lense.df = lensefun(d)  # for now, don't allow extra parameters, but require them to be built into lense function 
+  # returns data frame with value L and rowid ID
+  
+  # partition length = linear distance
+  pl = ( max(lense.df$L) - min(lense.df$L))/(l) *  (1+o) 
+  p0 = min(lense.df$L)
+  
+  partitions = list()
+  
+  # TODO: rewrite to use 'ldapply'  instead of forloop for when n > 10^6
+  for (i in 1:l) {
+    partition_start = p0 + pl * (i - 1) * (1-o)  # offset== starting value is 1/2 partition size X parttion number
+    partition_end   = partition_start + pl
+    partitions[[i]] = with(lense.df, lense.df[L >=  partition_start & L <= partition_end, "ID"])
+    # previously was partition of data: partitions[[i]] = d[lenses[[i]]$ID,]; now just an ID
+  }
+  # to test that all rows have been included in at least one partition
+  # if(nrow(d) != length(unique(unlist(partitions)))) stop("partitioning does not include all rows")
+  return(partitions)
+  
+}
+
+# creates a list of 'partitions' of dataset, using a reducing 'lense' function
+#' @export
+partition.graphmapper <- function(gm) {
+  if (class(gm) != "graphmapper") stop("partition: requires input of class graphmapper class")
+  gm[["partitions"]]  = partition(gm$d, gm$lensefun, gm$partition_count,  gm$overlap)
+} 
+
+
+# cluster detection using NbClust; 
+# the clustering built in so works on partitions
+#' @export
+clusters.graphmapper<- function(gm) {
+  index_method=gm$index_method
+  gmClusts = list()
+  distance_calc = "euclidean"
+  
+  # TODO: use ldapply instead of for loop
+  for ( i in 1:length(gm$partitions)) {
+    # if (debug) print(i)
+    # subset of data for this partition, which contains row names...
+    # this is not necessary here, and adds to R memory burden for large partitions
+    # but adds to code readability
+    d.subset = gm$d[partitions[[i]],]
+    nb = NbClust(as.matrix(d.subset), distance = distance_calc, method="single",
+            min.nc = 1, max.nc = 5,index =  index_method )
+    nbClusts[[i]] = nb$Best.partition
+  }
+  return(nbClusts)
+}
+
+# given a graph mapper object, create the nodes using the current clustered partitions
+# nodes are subsets of IDs (rownames) based on optimal partitions
+#' @export
+nodes.graphmapper <- function(gm){
+  l = length(gm$clusters)
+  nodes = list()
+  node_counter = 0
+  
+  # for each partition
+  for ( i in 1:l) {
+    
+    # shorten the name the item from the clusters list
+    cuts = clusters[[i]] 
+    
+    # cuts are a list of data points with associated cluster numbers, 1,1,2,2,...,k
+    # convert this repeating list of cluster numbers to just the numbers 1,2,..k
+    for(j in unique(cuts)){
+      # advance the node counter TODO: vectorize this loop
+      node_counter = node_counter + 1
+      data_subset_for_this_cluster = d[names(cuts[cuts == j]), ]  
+      # store these data inside this node
+      nodes[[node_counter]] = data_subset_for_this_cluster
+    }
+  }
+  return(nodes)
+}
+
+#' @export
+cedar.adj<- function(gm) {
+  nodes = gm$nodes
+  # function that tells if there is overlapping rows of data
+  detect_overlap <- function(a,b) { length(intersect(a$ID,b$ID)) }
+  
+  adjmat <- matrix(0, ncol = length(nodes), nrow = length(nodes))
+  colnames(adjmat) <- rownames(adjmat) <- names(nodes)
+  for(i in 1:nrow(adjmat)) {
+    for(j in 1:ncol(adjmat)) {
+      if (i == j) {
+        adjmat[i,j] = 0
+      } else {adjmat[i,j] <- detect_overlap(nodes[[i]], nodes[[j]])}
+    }
+  }
+  return(adjmat)
+}
+
+#' @export
+graph.graphmapper<- function(gm){
+  # need to use just upper half of matrix
+  adjmatrix[lower.tri(adjmatrix)] <- 0
+  g  <- graph_from_adjacency_matrix(adjmatrix, mode ="undirected",weighted="weight", diag=FALSE)
+  return(g)
+}
+
+# TODO: rename this function to reflect it's data prep
+#' @export
+cedar.graphprep = function(gm, varName="X"){
+  # converts an gm objects into structures with nodes and links
+  
+  # check that variable is in nodes
+  nodes = gm$nodes
+  # TODO: detect if adjmatrix is present, and if not, recreate
+  adjmatrix = gm$adjmatrix # cedar.adj(nodes)
+  
+
+  meanVariable <- function(litem) { mean(get(varName,litem))}
+  
+  # currently convert to lists to graph and then graph to lists
+  # TODO: cut out the middleman and prep from adjmatrix directly! 
+  g = cedar.graph(adjmatrix)
+  nodes_prepped = data.frame("name"  = as.vector((V(g))), 
+                             "values"= unlist(lapply(nodes,meanVariable)))
+  
+  links = get.data.frame(g)
+  links_prepped = data.frame(source=links$from - 1, target = links$to - 1 , weight = links$weight)
+  
+  return( list(graph_nodes = nodes_prepped, graph_links = links_prepped) )
+  
+}
+
+
+
+####### LENSES
+#' @export
+simple_lense = function(d,varname=NULL ){
+  # simple lense function that returns a single variable
+  # d = data as a data frame, varname  = name of variable as a string
+  
+  # if no variable name passed, use the first name
+  # requires data d to have named columns
+  if (is.null(varname)) { varname = names(d)[1] }
+  
+  # prep data frame for partitioning function with L column 
+  lense_data = data.frame(L=get(varname, d), ID=rownames(d), stringsAsFactors = FALSE)
+  return(lense_data)
+  
+}
+
+# simple lense on X coordinate
+#' @export
+x_lense <- function(d){
+  simple_lense(d, "X")
+}
+# simple lense on Y coordinate
+#' @export
+y_lense <- function(d){
+  simple_lense(d, "X")
+}
+
+
+### plotting 
+
+plot.graphmapper <- function(gm){
+  # create an edge list
+  adjmatrix = cedar.adj(gm)
+  d.graph   = cedar.graph(adjmatrix) 
+}
+
+plot_partitions <- function(partitions, xvar="X", yvar="Y")  {
+  par(mfrow=c(2,2))
+  
+  plot(d$X, d$Y, main="Unit Circle")
+  par(mfrow=c(2,2)) # set for 2X2 plot
+  for(i in 1:length(partitions)){ with(partitions[[i]], plot(X,Y)) }
+  par(mfrow=c(1,1))
+  
+  par(mfrow=c(2,2))
+  for(p in partitions){ 
+    # remove the ID column,TODO remove hard coded col num
+    d.subset = p[,-3]  
+    print( eclust(d.subset, FUNcluster="hclust", k.max = 5, stand =TRUE, B = 500, hc_metric="euclidean", hc_method="single"))
+  }
+}    
+
+plot_cluster = function(gm, cnumber){
+  cldata = cbind(d.partitions[[cnumber]],d.clusters[[cnumber]])
+  plot(cldata$X, cldata$Y, col = c("red", "green", "blue")[cldata$`d.clusters[[cnumber]]`])
+  # d[d$ID %in% names(cl[[1]]),]
+}
