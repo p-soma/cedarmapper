2741374f105f60af8a306e4f7225d0165667d7e7
Pat Bills <pat@billspat.com>
2016-06-28 06:49:01 -0400

    begin package documentation

---
+++ R/cedar.R
@@ -0,0 +1,12 @@
+#' cedar: A package for exploring data using the mapper topological analysis 
+#' described in Topological Methods for the Analysis of High Dimensional Data Sets and 3D Object Recognition (Singh et al 2007)
+#'
+#' The cedar package provides an object for storing results of the mapper pipeline, 
+#' a collection of filter functions or lenses for dimension reduction, 
+#' and a Shiny application to display the resulting graph
+
+#' @docType package
+#' @name cedar
+NULL
+
+# note this file currently serves only as the package documentation
