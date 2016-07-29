17b0523602087b22ccc6ab55318e979e57b9754a
Pat Bills <pat@billspat.com>
2016-07-29 00:57:17 -0400

    use makegraphmapper() for diabetes example

--- examples/example-diabdata.R
+++ examples/example-diabdata.R
@@ -5,28 +5,28 @@ data(chemdiab)
 # current must remove the classification variable
 chemdiab  <- scale(subset(chemdiab, select = -c(cc)))
 
-testlensefun = lense.density
-lenseparam = 1.0
+#testlensefun = lense.density
+# lenseparam = 1.0
 
 # alternative, single var projection
 # testlensefun = simple_lense
 # lenseparam = names(chemdiab)[1]
 
 
-gm  = graphmapper(dataset = chemdiab, 
-                    lensefun = testlensefun, 
+gm  <- makegraphmapper(dataset = chemdiab, 
+                    lensefun = lense.density, 
                     partition_count=8, 
                     overlap = 0.5, 
-                    lenseparam = lenseparam,
+                    lenseparam = 1.0,
                     bin_count=20 
                   )
 
 # create distance matrix for all data manually
-gm$distance = dist(gm$d,method="euclidean", upper=FALSE)
-gm$partitions = partition.graphmapper(gm)
-gm$clusters   = clusters.graphmapper(gm)
-gm$nodes      = nodes.graphmapper(gm)
-gm$adjmatrix  = adjacency.graphmapper(gm) 
+# gm$distance = dist(gm$d,method="euclidean", upper=FALSE)
+# gm$partitions = partition.graphmapper(gm)
+# gm$clusters   = clusters.graphmapper(gm)
+# gm$nodes      = nodes.graphmapper(gm)
+# gm$adjmatrix  = adjacency.graphmapper(gm) 
 
 plot(graph.graphmapper(gm),main=paste0("Diabetes Data, Density function"),sub=paste0("paritions: ", gm$partition_count, ", overlap: ", gm$overlap))
 
