f8af310883cd32cadcf236f74eebacc3ce84b0a4
Pat Bills <pat@billspat.com>
2017-02-14 12:50:53 -0500

    test setups

--- tests/testthat/test_mapper.R
+++ tests/testthat/test_mapper.R
@@ -1,16 +1,6 @@
 context("Mapper Object")
 
-### setup 
-example_lense  <- function(){
-  lense(lense.constant, lenseparam=NULL, partition_count=4, overlap = 0.5)
-}
-
-example_mapper <- function(){
-  mapper(dataset = circle_data(), 
-         lenses=list(example_lense()), 
-         cluster_method="single", bin_count=10, normalize_data=TRUE)
-}
-
+### helper functions are in helper_mapper.R file
 
 ### tests
 test_that("Mapper is object", {
