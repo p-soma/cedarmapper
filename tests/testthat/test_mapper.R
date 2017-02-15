99a789b23c7ed2099956198bfee0c3985ee2e929
Pat Bills <pat@billspat.com>
2017-02-14 20:09:49 -0500

    test of distance matrix calculation

--- tests/testthat/test_mapper.R
+++ tests/testthat/test_mapper.R
@@ -1,7 +1,7 @@
 context("Mapper Object")
 
 ### helper functions are in helper_mapper.R file
-
+d = circle_data()
 ### tests
 test_that("Mapper is object", {
   
@@ -9,5 +9,16 @@ test_that("Mapper is object", {
   expect_is(m, "mapper")
   expect_equal(m$bin_count,10)
 
-}
-)
+})
+
+
+test_that("mapper distance calc",{
+  m <- example_mapper()
+  m$distance <-  distance.mapper(m,method="euclidean")
+  expect_equal( class(m$distance), "dist")
+  expect_equal(attr(m$distance,"method"), "euclidean")
+  # not sure this will work, depends on circle data being used
+  # to 6 decimal places the same 
+  expect_equal(round(m$distance[1] - 0.1467897,7),0)
+})
+
