09f1ef2a998b2a17abf8a8577972f9d69bf9eff4
Pat Bills <pat@billspat.com>
2017-04-18 12:00:33 -0400

    new non-cedar example of semantic zoom

--- inst/examples/zoomandbrush.js
+++ inst/examples/zoomandbrush.js
@@ -1,19 +1,28 @@
+// THIS IS A SAMPLE FORCE LAYOUT THAT IMPLEMENTS FOLLOWING FEATURES:
+// semantic zoom:  when using the wheel to zoom, does not change pixels or nodes (e.g svg transform) but
+// zoom can move entire layout with mouse clicks
+// shift click (not on a node) will start a  rectangular brush that selects nodes not previously selected
+//   this brush compensates for current node position in space (e.g. if zoomed still selects correctly)
+// click on a nodes can be selected by clicking, clicking a node will de-selected all of the others currently selected
+// shift+click on a node will add to currently selected nodes
+// click and drag on a node will drag it and other layout will move out of the way
+// click and drag on a node will move ALL selected nodes in the same way
+// CURRENTLY NODES ARE NOT 'FIXED'  ; FORCE LAYOUT WILL MOVE THEM INTO POSITION
+
+
 function selectableForceDirectedGraph(el,w,h) {
-    var width,
-    height,
+    var force, width,height,
     shiftKey, ctrlKey;
 
-    // for semantic zoom
-    var scaleFactor = 1, translation = [0,0];
 
     width = w;
-    height = h;
+    height = 500;
 
     var nodeGraph = null;
-    var xScale = d3.scale.linear()
-    .domain([0,width]).range([0,width]);
-    var yScale = d3.scale.linear()
-    .domain([0,height]).range([0, height]);
+
+    // SCALES used by zoom
+    var xScale = d3.scale.linear().domain([0,width]).range([0,width]);
+    var yScale = d3.scale.linear().domain([0,height]).range([0, height]);
 
     var svg = d3.select(el)
     .attr("tabindex", 1)
@@ -24,25 +33,15 @@ function selectableForceDirectedGraph(el,w,h) {
     .attr("width", width)
     .attr("height", height);
 
-    // var zoomer = d3.behavior.zoom().
-    //     scaleExtent([0.1,10]).
-    //     x(xScale).
-    //     y(yScale).
-    //     on("zoomstart", zoomstart).
-    //     on("zoom", redraw);
-
-    // function zoomstart() {
-    //     node.each(function(d) {
-    //         d.selected = false;
-    //         d.previouslySelected = false;
-    //     });
-    //     node.classed("selected", false);
-    // }
-
-
     /*** Configure zoom behaviour ***/
+
+    // GLOBALS used by semantic zoom
+    var scaleFactor = 1, translation = [0,0];
+
     var zoomer = d3.behavior.zoom()
-        .scaleExtent([0.1,10]) //allow 10 times zoom in or out
+        .scaleExtent([0.1,10])
+        .x(xScale)
+        .y(yScale)
         .on("zoom", semanticzoom);
 
     function semanticzoom() {
@@ -56,29 +55,6 @@ function selectableForceDirectedGraph(el,w,h) {
         // vis.attr("transform",
         //          "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
     }
-
-    var brusher = d3.svg.brush()
-    //.x(d3.scale.identity().domain([0, width]))
-    //.y(d3.scale.identity().domain([0, height]))
-    .x(xScale)
-    .y(yScale)
-    .on("brushstart", function(d) {
-        node.each(function(d) {
-            d.previouslySelected = shiftKey && d.selected; });
-    })
-    .on("brush", function() {
-        var extent = d3.event.target.extent();
-
-        node.classed("selected", function(d) {
-            return d.selected = d.previouslySelected ^
-            (extent[0][0] <= d.x && d.x < extent[1][0]
-             && extent[0][1] <= d.y && d.y < extent[1][1]);
-        });
-    })
-    .on("brushend", function() {
-        d3.event.target.clear();
-        d3.select(this).call(d3.event.target);
-    });
 
     var svg_graph = svg.append('svg:g')
     .call(zoomer)
@@ -94,18 +70,34 @@ function selectableForceDirectedGraph(el,w,h) {
     //.attr("pointer-events", "all")
     .attr("id", "zrect")
 
+    // BRUSHING ********
+    var brusher = d3.svg.brush()
+      .x(d3.scale.identity().domain([0, width]))
+      .y(d3.scale.identity().domain([0, height]))
+      .on("brushstart", function(d) {
+        node.each(function(d) {
+            d.previouslySelected = shiftKey && d.selected; });
+          })
+    .on("brush", function() {
+        var extent = d3.event.target.extent();
+        console.log("**********extent x",extent[0][0],extent[1][0],"y",extent[0][1],extent[1][1]);
+
+        node.classed("selected", function(d) {
+              return( d.selected = d.previouslySelected ||
+            (   xScale.invert(extent[0][0]) <= d.x  &&  d.x < xScale.invert(extent[1][0])
+             && yScale.invert(extent[0][1]) <= d.y  &&  d.y < yScale.invert(extent[1][1])  )
+           );
+
+        });
+    })
+    .on("brushend", function() {
+        d3.event.target.clear();
+        d3.select(this).call(d3.event.target);
+    });
+
     var brush = svg_graph.append("g")
     .datum(function() { return {selected: false, previouslySelected: false}; })
     .attr("class", "brush");
-
-    var vis = svg_graph.append("svg:g");
-
-    vis.attr('fill', 'red')
-    .attr('stroke', 'black')
-    .attr('stroke-width', 1)
-    .attr('opacity', 0.5)
-    .attr('id', 'vis')
-
 
     brush.call(brusher)
     .on("mousedown.brush", null)
@@ -114,6 +106,15 @@ function selectableForceDirectedGraph(el,w,h) {
     .on("touchend.brush", null);
 
     brush.select('.background').style('cursor', 'auto');
+
+
+    var vis = svg_graph.append("svg:g");
+
+    vis.attr('fill', 'red')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('opacity', 0.5)
+    .attr('id', 'vis')
 
     var link = vis.append("g")
     .attr("class", "link")
@@ -124,6 +125,7 @@ function selectableForceDirectedGraph(el,w,h) {
     .selectAll("circle");
 
     center_view = function() {
+        //  THIS USING SVG TRANSFORMS, which we are trying to avoid
         // Center the view on the molecule(s) and scale it so that everything
         // fits in the window
 
@@ -165,7 +167,7 @@ function selectableForceDirectedGraph(el,w,h) {
         y_trans = -(min_y) * min_ratio + (height - new_mol_height) / 2;
 
 
-        // do the actual moving
+        // do the actual TRANSLATION
         vis.attr("transform",
                  "translate(" + [x_trans, y_trans] + ")" + " scale(" + min_ratio + ")");
 
@@ -176,14 +178,10 @@ function selectableForceDirectedGraph(el,w,h) {
 
     };
 
-    function dragended(d) {
-        //d3.select(self).classed("dragging", false);
-        node.filter(function(d) { return d.selected; })
-        .each(function(d) { d.fixed &= ~6; })
-
-    }
 
     function tick() {
+      // uses globals translation and scaleFactor, set by semantic Zoom fn
+
       link.attr("x1", function (d) {
               return translation[0] + scaleFactor*d.source.x;
           })
@@ -196,92 +194,41 @@ function selectableForceDirectedGraph(el,w,h) {
           .attr("y2", function (d) {
               return translation[1] + scaleFactor*d.target.y;
           });
-      node.attr("cx", function (d) {
 
-          //return(d.x)
-          return translation[0] + scaleFactor*d.x;
+
+      node.attr("cx", function (d) { return translation[0] + scaleFactor*d.x;
           })
-          .attr("cy", function (d) {
-              return translation[1] + scaleFactor*d.y;
+          .attr("cy", function (d) { return translation[1] + scaleFactor*d.y;
           });
     };
 
-    d3.json("diabetesmapper.json", function(error, graph) {
-        nodeGraph = graph;
-
-        graph.links.forEach(function(d) {
-            d.source = graph.nodes[d.source];
-            d.target = graph.nodes[d.target];
-        });
-
-        link = link.data(graph.links).enter().append("line")
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
 
 
-        var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(30)
-        .nodes(graph.nodes)
-        .links(graph.links)
-        .size([width, height])
-        .start();
+   // RESIZING
+   /* Set the display size based on the SVG size and re-draw */
+  function setSize() {
+    var svgStyles = window.getComputedStyle(svg.node());
+    var svgW = parseInt(svgStyles["width"]);
+    var svgH = parseInt(svgStyles["height"]);
 
-        function dragstarted(d) {
-            d3.event.sourceEvent.stopPropagation();
-            if (!d.selected && !shiftKey) {
-                // if this node isn't selected, then we have to unselect every other node
-                node.classed("selected", function(p) { return p.selected =  p.previouslySelected = false; });
-            }
+    //Set the output range of the scales
+    xScale.range([0, svgW]);
+    yScale.range([0, svgH]);
 
-            d3.select(this).classed("selected", function(p) { d.previouslySelected = d.selected; return d.selected = true; });
+    //re-attach the scales to the zoom behaviour
+    zoomer.x(xScale)
+          .y(yScale);
 
-            node.filter(function(d) { return d.selected; })
-            .each(function(d) { d.fixed |= 2; })
-        }
+    //resize the background
+    rect.attr("width", svgW)
+            .attr("height", svgH);
 
-        function dragged(d) {
-            node.filter(function(d) { return d.selected; })
-            .each(function(d) {
-                d.x += d3.event.dx;
-                d.y += d3.event.dy;
+    console.log(xScale.range(), yScale.range());
+    tick();//re-draw
 
-                d.px += d3.event.dx;
-                d.py += d3.event.dy;
-            })
+    }
 
-            force.resume();
-        }
-        node = node.data(graph.nodes).enter().append("circle")
-        .attr("r", 4)
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; })
-        .on("dblclick", function(d) { d3.event.stopPropagation(); })
-        .on("click", function(d) {
-            if (d3.event.defaultPrevented) return;
 
-            if (!shiftKey) {
-                //if the shift key isn't down, unselect everything
-                node.classed("selected", function(p) { return p.selected =  p.previouslySelected = false; })
-            }
-
-            // always select this node
-            d3.select(this).classed("selected", d.selected = !d.previouslySelected);
-        })
-
-        .on("mouseup", function(d) {
-            //if (d.selected && shiftKey) d3.select(this).classed("selected", d.selected = false);
-        })
-        .call(d3.behavior.drag()
-              .on("dragstart", dragstarted)
-              .on("drag", dragged)
-              .on("dragend", dragended));
-
-        force.on("tick", tick);
-
-    });
 
 
     function keydown() {
@@ -323,4 +270,98 @@ function selectableForceDirectedGraph(el,w,h) {
         brush.select('.background').style('cursor', 'auto')
         svg_graph.call(zoomer);
     }
+
+
+
+
+    function dragstarted(d) {
+        d3.event.sourceEvent.stopPropagation();
+        if (!d.selected && !shiftKey) {
+            // if this node isn't selected, then we have to unselect every other node
+            node.classed("selected", function(p) { return p.selected =  p.previouslySelected = false; });
+        }
+
+        d3.select(this).classed("selected", function(p) { d.previouslySelected = d.selected; return d.selected = true; });
+
+        node.filter(function(d) { return d.selected; })
+        .each(function(d) { d.fixed |= 2; })
+    }
+
+    function dragended(d) {
+        //d3.select(self).classed("dragging", false);
+        node.filter(function(d) { return d.selected; })
+        .each(function(d) { d.fixed &= ~6; })
+
+    }
+
+
+    function dragged(d) {
+        node.filter(function(d) { return d.selected; })
+        .each(function(d) {
+            d.x += d3.event.dx;
+            d.y += d3.event.dy;
+
+            d.px += d3.event.dx;
+            d.py += d3.event.dy;
+        })
+
+        force.resume();
+    }
+
+    // LOAD DATA, SETUP NODES AND LINKS,  AND START FORCE layout
+    d3.json("diabetesmapper.json", function(error, graph) {
+        nodeGraph = graph;
+
+        graph.links.forEach(function(d) {
+            d.source = graph.nodes[d.source];
+            d.target = graph.nodes[d.target];
+        });
+
+        link = link.data(graph.links).enter().append("line")
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+
+        force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(30)
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .size([width, height])
+        .start();
+
+
+        node = node.data(graph.nodes).enter().append("circle")
+        .attr("r", 4)
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .on("dblclick", function(d) { d3.event.stopPropagation(); })
+        .on("click", function(d) {
+                  if (d3.event.defaultPrevented) return;
+
+                  if (!shiftKey) {
+                      //if the shift key isn't down, unselect everything
+                      node.classed("selected", function(p) { return p.selected =  p.previouslySelected = false; })
+                  }
+
+                  // always select this node
+                  d3.select(this).classed("selected", d.selected = !d.previouslySelected);
+              })
+        .on("mouseup", function(d) {
+            //if (d.selected && shiftKey) d3.select(this).classed("selected", d.selected = false);
+            })
+        .call(d3.behavior.drag()
+              .on("dragstart", dragstarted)
+              .on("drag", dragged)
+              .on("dragend", dragended));
+
+
+        force.on("tick", tick);
+
+    });
+
+
+
 }
